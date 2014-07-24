c14e4cf796a980324af74986355aea7004890478
chasesmith95 <chase.smth@gmail.com>
2014-07-23 17:24:09 -0700

    The GUI can now transition between other frames

--- src/DesignOfExperimentsGUI.py
+++ src/DesignOfExperimentsGUI.py
@@ -6,91 +6,77 @@ Created on Jul 10, 2014
 from tkinter import *
 from tkinter import ttk
 
-
-
-
-class MainDialog:
-    def __init__(self, parent):
-        self.parent = parent
-        self.value_of_combo = 'Create New Experiment'
-        Label(self.parent, text="Choose Configuration: ", font=(12)).grid(column=2, row=3, padx=10, pady=10)
-        self.combo(('Create New Experiment', 'Load Previous Experiment'), 3, 3)
-        Label(self.parent, text="Choose File: ", font=(12)).grid(column=2, row=4, padx=10, pady=10)
-        self.entryValue=Entry(self.parent)
-        #self.directory = filedialog
-        #self.directory.askdirectory(initialdir='.')
-        self.entryValue.grid(column=3, row=4)
-        Label(self.parent, text="Choose Experiment Set-up: ", font=(12)).grid(column=2, row=5, padx=10, pady=10)
-        self.combo(('2 Level Fractional Factorial', 'Central Composite Design'), 5, 3)
+class MainDialog(ttk.tkinter.Tk):
+    def __init__(self):
+        ttk.tkinter.Tk.__init__(self)
         
-        self.nextButton= Button(self.parent, text="Next")
-        self.nextButton.grid(column=4, row=8, padx=5)
-        self.nextButton= Button(self.parent, text="Cancel", command=self.parent.quit)
-        self.nextButton.grid(column=3, row=8, padx=5)
-    
-    def newselection(self, event):
-        self.value_of_combo = self.box.get()
-        print(self.value_of_combo)
-        print(self.entryValue.get())
-#
-    def combo(self, values, row, col):
-        self.box_value = StringVar()
-        self.box = ttk.Combobox(self.parent, textvariable=self.box_value)
-        self.box.bind("<<ComboboxSelected>>", self.newselection)
-        self.box['values'] = values
-        self.box.current(0)
-        self.box.grid(column=col, row=row)
-    #def continueMain(self):
-      #  self.parent.
-
-    
-class CreateExperiment(Frame):
-  
-    def __init__(self, parent):
-        Frame.__init__(self, parent)   
-         
-        self.parent = parent        
-        self.initUI()
+        self.title("Design of Experiments")
+        self.loadExperiment=StringVar()
+        self.folderName=StringVar()
+        self.experimentName=StringVar()
+        self.experimentType=StringVar()
+        self.listFactors= StringVar()
+        self.listResults= StringVar()
+        self.numFactors=StringVar()
+        self.factorGenerator=StringVar()
+        self.listSignificantFactors=[]
+        self.listFactorWeights=[]
+        self.fracFactRunList=["5 Factor Design(16 Runs and Resolution V)", "9 Factor Design (32 Runs and Resolution IV)"]
+        self.createExperimentFrame= ttk.Frame(self)
+        self.initExperimentFrame= ttk.Frame(self)
+        self.makeCreateExperimentFrame(Shown=True)
         
-    def initUI(self):
-      
-        self.parent.title("Create Your Own Experiment") 
-        
-        self.pack(fill=BOTH, expand=1)
+       
 
-        runs = ['Two Level Fractional Factorial', '' ]
-
-        lb = Listbox(self)
-        for i in runs:
-            lb.insert(END, i)
+    def makeCreateExperimentFrame(self, Shown=False):
+        if(Shown==True):
+            self.title("Design of Experiments: Create Experiment")
+            self.geometry("550x200+350+350")
+            Label(self.createExperimentFrame, text="Choose Configuration: ", font=(12)).grid(column=2, row=3, padx=10, pady=10)
+            self.createExperimentFrame.experimentBox = ttk.Combobox(self.createExperimentFrame, textvariable=self.loadExperiment)
+            self.createExperimentFrame.experimentBox['values'] = ['Create New Experiment', 'Load Previous Experiment']
+            self.createExperimentFrame.experimentBox.current(0)
+            self.createExperimentFrame.experimentBox.grid(column=3, row=3)
+            Label(self.createExperimentFrame, text="Choose Folder: ", font=(12)).grid(column=2, row=4, padx=10, pady=10)
+            self.createExperimentFrame.entryValue=Entry(self.createExperimentFrame, textvariable=self.folderName)
+            self.createExperimentFrame.entryValue.grid(column=3, row=4)
+            Label(self.createExperimentFrame, text="Choose Experiment Set-up: ", font=(12)).grid(column=2, row=5, padx=10, pady=10)
+            self.createExperimentFrame.experimentTypeBox = ttk.Combobox(self.createExperimentFrame, textvariable=self.experimentType)
+            self.createExperimentFrame.experimentTypeBox['values'] = ['2 Level Fractional Factorial', 'Central Composite Design']
+            self.createExperimentFrame.experimentTypeBox.current(0)
+            self.createExperimentFrame.experimentTypeBox.grid(column=3, row=5)
+            self.createExperimentFrame.nextButton= Button(self.createExperimentFrame, text="Next", command=self.goToInitExperiment)
+            self.createExperimentFrame.nextButton.grid(column=4, row=8, padx=5)
+            self.createExperimentFrame.cancelButton= Button(self.createExperimentFrame, text="Cancel", command=self.quit)
+            self.createExperimentFrame.cancelButton.grid(column=3, row=8, padx=5)
+            self.createExperimentFrame.pack()
             
-        lb.bind("<<ListboxSelect>>", self.onSelect)    
+    def goToInitExperiment(self):
+        self.createExperimentFrame.pack_forget()
+        self.initExperimentFrame=self.makeInitExperimentFrame(Shown=True)
+        
+    def makeInitExperimentFrame(self, Shown=False):
+        if(Shown==True):
+            self.title("Design of Experiments: Initialize Fractional Factorial")
+            self.geometry("500x500+350+350")  
+            if(self.experimentType.get()=='2 Level Fractional Factorial'):
+                
+                Label(self.initExperimentFrame, text="Choose Number of Factors: ", font=(12)).grid(column=2, row=3, padx=10, pady=10)
+                self.initExperimentFrame.factorSpin=Spinbox(self.initExperimentFrame, textvariable=self.numFactors, from_ = 5, to = 15)
+                self.initExperimentFrame.factorSpin.grid(column=3, row=3)
+                Label(self.initExperimentFrame, text="Choose Design: ", font=(12)).grid(column=2, row=4, padx=10, pady=10)
+                self.initExperimentFrame.experimentBox = ttk.Combobox(self.initExperimentFrame, textvariable=self.factorGenerator)
+                self.initExperimentFrame.experimentBox['values'] = self.fracFactRunList
+                self.initExperimentFrame.experimentBox.current(0)
+                self.initExperimentFrame.experimentBox.grid(column=3, row=4)
             
-        lb.place(x=20, y=20)
-
-        self.var = StringVar()
-        self.label = Label(self, text=0, textvariable=self.var)        
-        self.label.place(x=20, y=210)
-
-    def onSelect(self, val):
-      
-        sender = val.widget
-        idx = sender.curselection()
-        value = sender.get(idx)   
-
-        self.var.set(value)
-         
-
-def main():
-  
-    root = Tk()
-    ex = MainDialog(root)
-    root.geometry("550x200+350+350")
-    root.mainloop()  
-
-
-if __name__ == '__main__':
-    main()  
-
-
-
+            Label(self.initExperimentFrame, text="Choose Experiment Name: ", font=(12)).grid(column=2, row=1, padx=10, pady=10)  
+            self.initExperimentFrame.entryValue=Entry(self.initExperimentFrame, textvariable=self.experimentName)
+            self.initExperimentFrame.entryValue.grid(column=3, row=1)
+            Label(self.initExperimentFrame, text="List Variables: ", font=(12)).grid(column=2, row=2, padx=10, pady=10)  
+            self.initExperimentFrame.entryValue=Entry(self.initExperimentFrame, textvariable=self.listFactors)
+            self.initExperimentFrame.entryValue.grid(column=3, row=2)
+            self.initExperimentFrame.pack()
+            
+myGui= MainDialog()
+myGui.mainloop()
